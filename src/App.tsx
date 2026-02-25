b4aada3a383833fa493d4fe5cbd55987b9f6961b
YuriiMat <yurii.matviichuk@sigma.software>
2026-02-25 12:50:27 +0200

    Task 1. Create a sidebar with filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
-function App() {
-  return <></>;
-}
+import { useState } from 'react';
+import Sidebar from './layouts/Sidebar';
 
-export default App;
+const EMPTY_SIDEBAR_FORM = {
+  category: '',
+  genre: '',
+  releaseYear: '',
+  rating: 0,
+  language: '',
+};
+
+export default function App() {
+  const [isSidebarOpened, setIsSidebarOpened] = useState(false);
+  const [sidebarForm, setSidebarForm] = useState(EMPTY_SIDEBAR_FORM);
+
+  function handleSidebarFormChange<T>(name: string, value: T) {
+    setSidebarForm((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleClearFilters(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setSidebarForm(EMPTY_SIDEBAR_FORM);
+  }
+
+  return (
+    <div className="flex h-screen w-full overflow-x-hidden bg-gray-900">
+      <main className="flex-1 text-amber-50">
+        {JSON.stringify(sidebarForm)}
+      </main>
+      <Sidebar
+        handleClearFilters={handleClearFilters}
+        sidebarForm={sidebarForm}
+        isOpen={isSidebarOpened}
+        toggleSidebar={() => setIsSidebarOpened((prev) => !prev)}
+        onFilterChange={handleSidebarFormChange}
+        onRatingChange={handleSidebarFormChange}
+      />
+    </div>
+  );
+}
